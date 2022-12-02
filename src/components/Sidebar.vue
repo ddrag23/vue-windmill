c78c1c2d3c1aec290a6b37d51570c7d08f14e32e
ddrag23 <kurodyami@gmail.com>
2022-12-03 06:14:36 +0700

    initial commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import MenuItem from './MenuItem.vue'
+
+const isPagesMenuOpen = ref(false);
+function togglePagesMenu(): void {
+  isPagesMenuOpen.value = !isPagesMenuOpen.value;
+}
+const menus = ref([
+    {
+        link : '/about',
+        menu_name : "Tentang",
+    },
+    {
+        link : '/about',
+        menu_name : "Tentang",
+        isDropdown:true,
+        sub_menu :[
+           { link : '/about',
+        menu_name : "Tentang",}
+        ]
+    },
+])
+</script>
+<template>
+  <aside
+    class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0"
+  >
+    <div class="py-4 text-gray-500 dark:text-gray-400">
+      <a
+        class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
+        href="#"
+      >
+        Vue Windmill
+      </a>
+      <ul class="mt-6">
+        <li class="relative px-6 py-3">
+          <!-- Active items have the snippet below -->
+          <!-- <span
+                class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"
+              ></span> -->
+
+          <!-- Add this classes to an active anchor (a tag) -->
+          <!-- text-gray-800 dark:text-gray-100 -->
+         <MenuItem link="/dashboard" menuName="Dashboard"/>
+        </li>
+      </ul>
+      <ul>
+      <template v-for="menu in menus">
+            
+        <li class="relative px-6 py-3" v-if="!menu.hasOwnProperty('isDropdown')">
+         <MenuItem :link="menu.link" :menuName="menu.menu_name"/>
+        </li>
+        <li class="relative px-6 py-3" v-else>
+          <button
+            class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+            @click="togglePagesMenu"
+            aria-haspopup="true"
+          >
+            <span class="inline-flex items-center">
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
+                ></path>
+              </svg>
+              <span class="ml-4">{{ menu.menu_name }}</span>
+            </span>
+            <svg
+              class="w-4 h-4"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <template v-if="isPagesMenuOpen">
+            <ul
+              class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
+              aria-label="submenu"
+            >
+              <li
+                class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                v-for="subMenu in menu.sub_menu"
+              >
+                <router-link class="w-full" :to="subMenu.link">{{ subMenu.menu_name  }}</router-link>
+              </li>
+            </ul>
+          </template>
+        </li>
+      </template>
+        
+      </ul>
+      <div class="px-6 my-6">
+        <button
+          class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        >
+          Create account
+          <span class="ml-2" aria-hidden="true">+</span>
+        </button>
+      </div>
+    </div>
+  </aside>
+</template>
---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import Sidebar from "@/components/Sidebar.vue";
+import Header from "@/components/Header.vue";
+</script>
+<template>
+  <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
+    <Sidebar />
+    <div class="flex flex-col flex-1">
+      <Header />
+      <main class="h-full pb-16 overflow-y-auto">
+        <!-- Remove everything INSIDE this div to a really blank page -->
+        <div class="container px-6 mx-auto grid">
+          <RouterView />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
