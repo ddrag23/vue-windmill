002b11d7aad3273035513b2b7e4088e3d91d4991
ddrag23 <kurodyami@gmail.com>
2022-12-04 07:14:23 +0700

    remove alpine syntax

--- src/components/MobileSidebar.vue
+++ src/components/MobileSidebar.vue
@@ -16,12 +16,6 @@ function togglePagesMenu(): void {
   <Transition>
     <div
       v-show="isOpenNavbar"
-      x-transition:enter="transition ease-in-out duration-150"
-      x-transition:enter-start="opacity-0"
-      x-transition:enter-end="opacity-100"
-      x-transition:leave="transition ease-in-out duration-150"
-      x-transition:leave-start="opacity-100"
-      x-transition:leave-end="opacity-0"
       class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"
     ></div>
   </Transition>
@@ -29,12 +23,6 @@ function togglePagesMenu(): void {
     <aside
       class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
       v-show="isOpenNavbar"
-      x-transition:enter="transition ease-in-out duration-150"
-      x-transition:enter-start="opacity-0 transform -translate-x-20"
-      x-transition:enter-end="opacity-100"
-      x-transition:leave="transition ease-in-out duration-150"
-      x-transition:leave-start="opacity-100"
-      x-transition:leave-end="opacity-0 transform -translate-x-20"
       @keydown.esc="isOpenNavbar = false"
     >
       <div class="py-4 text-gray-500 dark:text-gray-400">
